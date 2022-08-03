390abe3cd3d051bf9ea05f4a30be8ee2689442b4
Sannithpeddolla <sannithpeddolla143@gmail.com>
2022-08-03 12:46:55 +0530

    adding css

---
+++ myfirstapp/src/components/Header.js
@@ -0,0 +1,26 @@
+import React,{Fragment,Component} from 'react';
+import './Header.css';
+class Header extends Component{
+    render(){
+        return(
+            <Fragment>
+                <header>  
+                    <div className='logo'>ReactApp...!</div>
+                        <center>
+                   
+                    <span className='text'>SerachBar</span>
+                    <input type="text"/>
+                </center>
+                </header>
+              
+                <hr/>
+                <hr/>
+            </Fragment>
+        )
+
+    }
+}
+
+
+
+export default Header;

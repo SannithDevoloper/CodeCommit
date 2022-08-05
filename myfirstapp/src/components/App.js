57b8e3effaf478b6c4290084f02f096ed06378c5
Sannithpeddolla <sannithpeddolla143@gmail.com>
2022-08-05 12:42:02 +0530

    try

--- myfirstapp/src/components/App.js
+++ myfirstapp/src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import ProductDisplay from './ProductDisplay';
-import JSON from './db.json'
+import JSON from './db2.json'
 class App extends Component {
     constructor(props) {
       super(props)
@@ -13,8 +13,11 @@ class App extends Component {
     }
     
     render(){return (
-        <div>
-            <Header userInput={(data) => {console.log('inside home',data)}}/>
+      //in header component props is assigned from child to parent
+    
+        <div style={({backgroundColor:'white'})}>
+            <Header userInput={(data) => {console.log('inside home',data)}}/> 
+            
             <ProductDisplay prodData={this.state.productData}/>
             <Footer year='2022' month='august'/>
         </div>
--- myfirstapp/src/components/ProductDisplay.js
+++ myfirstapp/src/components/ProductDisplay.js
@@ -10,13 +10,13 @@ const ProductDisplay=(props)=> {
     const renderProduct=props.prodData.map((item) =>{
 
       return(
-        <div className="card">
+        <div className="card" >
         <img src={item.image} alt={item.name}/>
         <div>
             <h3>{item.name}</h3>
             <p>{item.description}</p>
             <p>Rs.{item.cost}</p>
-            <p>{item.uses}</p>
+          
         </div>
     </div>
 

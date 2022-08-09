ce199e076ccf72ae2f552146a06b422383300487
Sannithpeddolla <sannithpeddolla143@gmail.com>
2022-08-09 23:26:47 +0530

    Routing

---
+++ routing5/src/Components/Post.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Post = () => {
+    return(
+        <div className="panel panel-success">
+            <div className="panel-heading">
+                Post Page
+            </div>
+            <div className="panel-body">
+                Post Page is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen  
+                Post Page is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen  
+        
+           
+            </div>
+        </div>
+    )
+}
+
+export default Post;
---
+++ routing5/src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routing from './Components/Routing';
+ReactDOM.render(<Routing/>,document.getElementById('root'));
